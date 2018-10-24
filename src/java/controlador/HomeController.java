f22d5d1ae8cebadd2c89660e5d81f877708342f0
moises <mo.sepulvedar@profesor.duoc.cl>
2018-10-23 22:40:45 -0300

    primera clase: creacion de proyecto y configuracion del mismo, creando un home controller

---
+++ src/java/controlador/HomeController.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author Duoc
+ */
+@Controller
+public class HomeController {
+    
+    @RequestMapping(value="/home", method = RequestMethod.GET)
+    public String home(Model model) {
+         
+        model.addAttribute("mensaje", "saludo desde el controlador");
+        
+        return "index";
+    }
+    
+}
